f5d4d25c3fa39105b33b00c3c92d70be2d4593d3
Himanshu Nachane <himanshu.n1495@gmail.com>
2017-07-22 00:48:09 +0530

    Function to check today's tasks added along with colors

--- todo.py
+++ todo.py
@@ -1,9 +1,36 @@
+
 import csv
+import datetime
+from termcolor import colored
 
-def on_start():
-	#User Greeting 
-	print "Welcome to TODO!"
+#function to check todays tasks
+def tasks_today():
+	fileOpen = open('data.csv') #open the file
+	fileRead = csv.reader(fileOpen) #read the file
+	dataFile = list(fileRead) #create list of items in file
+	tasks_today = [x for x in dataFile if x[2]==str(datetime.date.today())] #filter todays task using date
 
-on_start()
+	if len(tasks_today) > 0: 
+		#if tasks are present, display them
+		print colored("Today's tasks : ",'green')
+		print colored("Task ID    |     Task    |       Date",'yellow')
+		print colored("-----------------------------------------",'yellow')			
+		for task in tasks_today:
+			print colored("    " + task[0] + "      |    " + task[1] + "    |    " + task[2],'cyan')
+		return
+	else:
+		#if no tasks are present 
+		print colored("There are no tasks today!",'green')		
+		return
+	return	
+#User Greeting 
+print colored("Welcome to TODO!",'cyan')
+print " "
+
+tasks_today()
+#def on_start():
+	
+	
+#on_start()
 
 		
